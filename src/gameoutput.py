dec2afdf6ccf2ee7d8afd5eeb54b01d2fb66340d
Alexander Oprisnik <alexoprisnik@gmail.com>
2012-11-02 22:35:32 +0100

    test game output

--- src/gameoutput.py
+++ src/gameoutput.py
@@ -1,10 +1,60 @@
+#import direct.directbase.DirectStart
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import CollisionTraverser,CollisionNode
+from panda3d.core import CollisionHandlerQueue,CollisionRay
+from panda3d.core import Filename,AmbientLight,DirectionalLight
+from panda3d.core import PandaNode,NodePath,Camera,TextNode
+from panda3d.core import Vec3,Vec4,BitMask32
+from direct.gui.OnscreenText import OnscreenText
+from direct.actor.Actor import Actor
 from direct.showbase.DirectObject import DirectObject
-from direct.task import Task
+from direct.showbase import Audio3DManager
+import random, sys, os, math
+from player import Player
 
-class GameOutput(DirectObject):
+CAM_HEIGHT = 25
+CAM_ANGLE = 45
 
-    def __init__(self):
-        DirectObject.__init__(self)
+# Function to put title on the screen.
+def addTitle(text):
+    return OnscreenText(text=text, style=1, fg=(1,1,1,1),
+                        pos=(1.3,-0.95), align=TextNode.ARight, scale = .07)
+
+class GameOutput(ShowBase):
+
+    def __init__(self, level):
+        ShowBase.__init__(self)
+        
+        # Load the environment model.
+        self.environ = loader.loadModel(level)
+        self.environ.reparentTo(render)
+        #self.environ.setScale(0.25, 0.25, 0.25)
+        #self.environ.setPos(-8, 42, 0)
+        base.win.setClearColor(Vec4(0,0,0,1))
+        self.title = addTitle("PushBack")
+        
+        self.player = Player()
+        self.player.find("**/+GeomNode").setH(180)
+        self.player.reparentTo(render)
+        base.disableMouse()
+        base.cam.reparentTo(self.player)
+        base.cam.setCompass()
+        base.cam.setH(0)
+        base.cam.setZ(CAM_HEIGHT)
+        base.cam.setY(-CAM_HEIGHT)
+        base.cam.setP(-CAM_ANGLE)
+        
+        ambientLight = AmbientLight("ambientLight")
+        ambientLight.setColor(Vec4(.3, .3, .3, 1))
+        directionalLight = DirectionalLight("directionalLight")
+        directionalLight.setDirection(Vec3(-5, -5, -5))
+        directionalLight.setColor(Vec4(1, 1, 1, 1))
+        directionalLight.setSpecularColor(Vec4(1, 1, 1, 1))
+        render.setLight(render.attachNewNode(ambientLight))
+        render.setLight(render.attachNewNode(directionalLight))
 
     def start(self):
         pass
+        
+go = GameOutput("../resources/level/testlevel_new.x")
+run()
---
+++ src/player.py
@@ -0,0 +1,110 @@
+import direct.directbase.DirectStart
+from direct.actor.Actor import Actor
+
+PUSHY_PATH = "../resources/character/friday_2106/"
+class Player(Actor):
+    def __init__(self):
+        Actor.__init__(self,PUSHY_PATH+"pushy.x",
+                        {"charge":PUSHY_PATH+"pushy_charge.x",
+                        "charge_fly":PUSHY_PATH+"pushy_charge_fly.x",
+                        "charge_hit":PUSHY_PATH+"pushy_charge_hit.x",
+                        "charge_miss":PUSHY_PATH+"pushy_charge_miss.x",
+                        "charge_release":PUSHY_PATH+"pushy_charge_release.x",
+                        "charge_start":PUSHY_PATH+"pushy_charge_start.x",
+                        "fall":PUSHY_PATH+"pushy_fall.x",
+                        "run":PUSHY_PATH+"pushy_run.x",
+                        "run_start":PUSHY_PATH+"pushy_run_start.x",
+                        "standup":PUSHY_PATH+"pushy_standup.x",
+                        "stop":PUSHY_PATH+"pushy_stop.x",
+                        "walk":PUSHY_PATH+"pushy_walk.x"})
+        self.setScale(0.2)
+        self.setPlayRate(0.05, "fall")
+        self.setPlayRate(0.05, "charge_hit")
+        self.setPlayRate(0.05, "charge_miss")
+        self.setPlayRate(0.03, "standup")
+        self.setPlayRate(0.05, "charge_release")
+        self.setPlayRate(0.1, "run")   
+        self.isMoving = False
+        self.isFalling = False
+        self.isCharging = False
+        self.isFlying = False
+        self.flyingTime = 0
+        self.acFall=self.getAnimControl("fall")
+        self.acStandup=self.getAnimControl("standup")
+        
+        self.rotationSpeed = 300
+        self.movementSpeed = 25
+        self.movementSpeedFlying = 50
+        self.movementSpeedFalling = 50
+        
+    def updateAnimation(self):
+        if self.isMoving == True:
+            self.loop("run")
+        else:
+            self.stop()
+            self.pose("charge",5)
+        
+    def rotateLeft(self):
+        self.setH(self.getH() + self.rotationSpeed * globalClock.getDt())
+        if self.isMoving == False:
+            self.isMoving = True
+            self.updateAnimation()
+        
+    def rotateRight(self):
+        self.setH(self.getH() - self.rotationSpeed * globalClock.getDt())
+        if self.isMoving == False:
+            self.isMoving = True
+            self.updateAnimation()
+        
+    def moveForward(self):
+        self.setY(self, - self.movementSpeed * globalClock.getDt())
+        if self.isMoving == False:
+            self.isMoving = True
+            self.updateAnimation()
+        
+    def moveBackward(self):
+        self.setY(self, self.movementSpeed * globalClock.getDt())
+        if self.isMoving == False:
+            self.isMoving = True
+            self.updateAnimation()
+        
+    def stopMoving(self):
+        if self.isMoving == True:
+            self.isMoving = False
+            self.updateAnimation()
+        
+    def charge(self):
+        #self.setY(self, -3 * globalClock.getDt())
+        if(self.isCharging == False):
+            self.isCharging = True
+            self.loop("charge")
+        else:
+            self.flyingTime = self.flyingTime + 1;
+
+        
+    def stopCharge(self):
+        if(self.isCharging == True):
+            self.isCharging = False
+            self.loop("charge_fly")
+            self.isFlying = True
+        if(self.isFlying == True):
+            self.flyingTime = self.flyingTime-1
+            self.setY(self, -self.movementSpeedFlying * globalClock.getDt())
+            if(self.flyingTime <= 0):
+                self.isFlying = False
+                self.play("charge_miss")
+        
+        
+    def fall(self):
+        if(self.isFalling == False and self.acStandup.isPlaying() == False):
+            self.isFalling = True
+            self.acFall.play()
+        if(self.acFall.isPlaying() == True):
+            self.setY(self, +self.movementSpeedFalling * globalClock.getDt())
+        else:
+            if  self.acStandup.isPlaying() == False:
+                self.acStandup.play()
+            else:
+                self.isFalling = False
+
+        
